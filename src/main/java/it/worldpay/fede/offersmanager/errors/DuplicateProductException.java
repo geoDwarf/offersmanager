fc15c49c2da74c4633719449c4abe4161692ed12
geoDwarf <fedo.baiocco@gmail.com>
2018-10-01 22:38:31 +0200

    exception classes

---
+++ src/main/java/it/worldpay/fede/offersmanager/errors/DuplicateProductException.java
@@ -0,0 +1,5 @@
+package it.worldpay.fede.offersmanager.errors;
+
+public class DuplicateProductException extends RuntimeException{
+
+}
---
+++ src/main/java/it/worldpay/fede/offersmanager/errors/ProductNotFoundException.java
@@ -0,0 +1,5 @@
+package it.worldpay.fede.offersmanager.errors;
+
+public class ProductNotFoundException extends RuntimeException{
+
+}
